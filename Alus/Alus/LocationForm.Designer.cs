ce60f501ab6333505351ec4abb8a1e906869ce98
Andrius Bentkus <andrius.bentkus@gmail.com>
2017-10-12 11:55:26 +0300

    Tidy up LocationForm

--- Alus/Alus/LocationForm.Designer.cs
+++ Alus/Alus/LocationForm.Designer.cs
@@ -38,56 +38,56 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 63);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(433, 404);
+            this.pictureBox1.Size = new System.Drawing.Size(433, 420);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 4);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 40);
+            this.button1.Size = new System.Drawing.Size(128, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Nustatyti buvimo vietą";
+            this.button1.Text = "Determine position";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(519, 11);
+            this.button2.Location = new System.Drawing.Point(518, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(114, 43);
+            this.button2.Size = new System.Drawing.Size(114, 23);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Atgal";
+            this.button2.Text = "Main menu";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(157, 9);
+            this.label1.Location = new System.Drawing.Point(146, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(327, 13);
+            this.label1.Size = new System.Drawing.Size(187, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Norėdami priartinti/atitolinti nuspauskite ctrl ir naudokite pelės ratuką";
+            this.label1.Text = "Use ctrl with mouse wheel for zooming";
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(455, 64);
+            this.listBox1.Location = new System.Drawing.Point(455, 41);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(177, 407);
+            this.listBox1.Size = new System.Drawing.Size(177, 420);
             this.listBox1.TabIndex = 4;
             this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
             this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
             // 
-            // Form3
+            // LocationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(643, 479);
+            this.ClientSize = new System.Drawing.Size(643, 472);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -95,7 +95,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.KeyPreview = true;
-            this.Name = "Form3";
+            this.Name = "LocationForm";
             this.Text = "Location";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form3_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form3_KeyUp);
