ec4789d5c8fb95b8cb6db528f883f7b92573840e
Andrius Bentkus <andrius.bentkus@stud.mif.vu.lt>
2017-10-16 18:24:19 +0300

    Tidy up feedback form
    * Set a title for the feedback form
    * make sizes of buttons similar
    * Call the button from the main menu "Feedback" as is the form called
    * anchor so the feedback form is resizable

--- Alus/Alus/MainForm.Designer.cs
+++ Alus/Alus/MainForm.Designer.cs
@@ -38,9 +38,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 41);
+            this.button1.Location = new System.Drawing.Point(24, 79);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.Size = new System.Drawing.Size(262, 44);
             this.button1.TabIndex = 0;
             this.button1.Text = "Bar evaluation";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,9 +49,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Location = new System.Drawing.Point(24, 23);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 23);
+            this.button2.Size = new System.Drawing.Size(262, 44);
             this.button2.TabIndex = 1;
             this.button2.Text = "Beerglass level";
             this.button2.UseVisualStyleBackColor = true;
@@ -58,9 +60,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Location = new System.Drawing.Point(24, 135);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(131, 23);
+            this.button3.Size = new System.Drawing.Size(262, 44);
             this.button3.TabIndex = 2;
             this.button3.Text = "Nearest bars";
             this.button3.UseVisualStyleBackColor = true;
@@ -68,9 +71,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 158);
+            this.button4.Location = new System.Drawing.Point(24, 304);
+            this.button4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(131, 23);
+            this.button4.Size = new System.Drawing.Size(262, 44);
             this.button4.TabIndex = 3;
             this.button4.Text = "Exit";
             this.button4.UseVisualStyleBackColor = true;
@@ -78,20 +82,21 @@
             // 
             // suggestions
             // 
-            this.suggestions.Location = new System.Drawing.Point(12, 98);
-            this.suggestions.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.suggestions.Location = new System.Drawing.Point(24, 188);
+            this.suggestions.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.suggestions.Name = "suggestions";
-            this.suggestions.Size = new System.Drawing.Size(131, 25);
+            this.suggestions.Size = new System.Drawing.Size(262, 48);
             this.suggestions.TabIndex = 4;
-            this.suggestions.Text = "Suggestions";
+            this.suggestions.Text = "Feedback";
             this.suggestions.UseVisualStyleBackColor = true;
             this.suggestions.Click += new System.EventHandler(this.suggestions_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(12, 129);
+            this.button6.Location = new System.Drawing.Point(24, 248);
+            this.button6.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(131, 23);
+            this.button6.Size = new System.Drawing.Size(262, 44);
             this.button6.TabIndex = 5;
             this.button6.Text = "Statistical Table";
             this.button6.UseVisualStyleBackColor = true;
@@ -99,15 +104,16 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(152, 190);
+            this.ClientSize = new System.Drawing.Size(464, 365);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.suggestions);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "MainForm";
             this.Text = "Form2";
             this.ResumeLayout(false);
