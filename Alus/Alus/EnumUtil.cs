bae6cb4ba0de0b71f8a51addfb94463e47159b75
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2017-10-17 00:04:11 +0300

    Add better feedback functionality
    at least writes to a file now, has an interface which is overloadable
    also enums are used

---
+++ Alus/Alus/EnumUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Linq;
+
+namespace Alus
+{
+    public static class EnumUtil
+    {
+        public static IEnumerable<T> GetValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+
+        public static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+    }
+}
--- Alus/Alus/FeedbackForm.Designer.cs
+++ Alus/Alus/FeedbackForm.Designer.cs
@@ -39,19 +39,19 @@
             // emailLabel
             // 
             this.emailLabel.AutoSize = true;
-            this.emailLabel.Location = new System.Drawing.Point(8, 9);
-            this.emailLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.emailLabel.Location = new System.Drawing.Point(11, 5);
+            this.emailLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.emailLabel.Name = "emailLabel";
-            this.emailLabel.Size = new System.Drawing.Size(198, 25);
+            this.emailLabel.Size = new System.Drawing.Size(96, 13);
             this.emailLabel.TabIndex = 0;
             this.emailLabel.Text = "Your email address";
             // 
             // emailTextBox
             // 
-            this.emailTextBox.Location = new System.Drawing.Point(13, 39);
-            this.emailTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.emailTextBox.Location = new System.Drawing.Point(11, 21);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.emailTextBox.Name = "emailTextBox";
-            this.emailTextBox.Size = new System.Drawing.Size(300, 31);
+            this.emailTextBox.Size = new System.Drawing.Size(152, 20);
             this.emailTextBox.TabIndex = 1;
             // 
             // feedbackTextBox
@@ -59,36 +59,30 @@
             this.feedbackTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.feedbackTextBox.Location = new System.Drawing.Point(13, 80);
-            this.feedbackTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.feedbackTextBox.Location = new System.Drawing.Point(11, 46);
+            this.feedbackTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.feedbackTextBox.Multiline = true;
             this.feedbackTextBox.Name = "feedbackTextBox";
-            this.feedbackTextBox.Size = new System.Drawing.Size(856, 483);
+            this.feedbackTextBox.Size = new System.Drawing.Size(419, 304);
             this.feedbackTextBox.TabIndex = 2;
             // 
             // feedbackComboBox
             // 
             this.feedbackComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.feedbackComboBox.FormattingEnabled = true;
-            this.feedbackComboBox.Items.AddRange(new object[] {
-            "General feedback",
-            "Suggestions",
-            "Bugs",
-            "Complaints",
-            "Other"});
-            this.feedbackComboBox.Location = new System.Drawing.Point(569, 37);
-            this.feedbackComboBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.feedbackComboBox.Location = new System.Drawing.Point(278, 21);
+            this.feedbackComboBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.feedbackComboBox.Name = "feedbackComboBox";
-            this.feedbackComboBox.Size = new System.Drawing.Size(300, 33);
+            this.feedbackComboBox.Size = new System.Drawing.Size(152, 21);
             this.feedbackComboBox.TabIndex = 3;
             // 
             // sendButton
             // 
             this.sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.sendButton.Location = new System.Drawing.Point(669, 573);
-            this.sendButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sendButton.Location = new System.Drawing.Point(330, 356);
+            this.sendButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.sendButton.Name = "sendButton";
-            this.sendButton.Size = new System.Drawing.Size(200, 44);
+            this.sendButton.Size = new System.Drawing.Size(100, 23);
             this.sendButton.TabIndex = 4;
             this.sendButton.Text = "Send";
             this.sendButton.UseVisualStyleBackColor = true;
@@ -97,10 +91,10 @@
             // suggestionExitButton
             // 
             this.suggestionExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.suggestionExitButton.Location = new System.Drawing.Point(13, 573);
-            this.suggestionExitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.suggestionExitButton.Location = new System.Drawing.Point(11, 356);
+            this.suggestionExitButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.suggestionExitButton.Name = "suggestionExitButton";
-            this.suggestionExitButton.Size = new System.Drawing.Size(200, 44);
+            this.suggestionExitButton.Size = new System.Drawing.Size(100, 23);
             this.suggestionExitButton.TabIndex = 5;
             this.suggestionExitButton.Text = "Exit";
             this.suggestionExitButton.UseVisualStyleBackColor = true;
@@ -108,16 +102,16 @@
             // 
             // FeedbackForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(882, 631);
+            this.ClientSize = new System.Drawing.Size(441, 391);
             this.Controls.Add(this.suggestionExitButton);
             this.Controls.Add(this.sendButton);
             this.Controls.Add(this.feedbackComboBox);
             this.Controls.Add(this.feedbackTextBox);
             this.Controls.Add(this.emailTextBox);
             this.Controls.Add(this.emailLabel);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "FeedbackForm";
             this.Text = "Feedback";
             this.ResumeLayout(false);
