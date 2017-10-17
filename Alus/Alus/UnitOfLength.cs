e6d8eb6829c40279e5122888c5ccf6e25f43a3f5
Andrius Bentkus <andrius.bentkus@gmail.com>
2017-10-17 10:33:50 +0300

    std interface implementation, IComparer<T>
    The class compares LocationDistanceComparer the length of two other
    locations relatively to a third one (which is passed to the
    constructor of this class).

---
+++ Alus/Alus/UnitOfLength.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alus
+{
+    public class UnitOfLength
+    {
+        public static readonly UnitOfLength Kilometers = new UnitOfLength(1.609344);
+        public static readonly UnitOfLength NauticalMiles = new UnitOfLength(0.8684);
+        public static readonly UnitOfLength Miles = new UnitOfLength(1);
+
+        private readonly double _fromMilesFactor;
+
+        private UnitOfLength(double fromMilesFactor)
+        {
+            _fromMilesFactor = fromMilesFactor;
+        }
+
+        public double ConvertFromMiles(double input)
+        {
+            return input * _fromMilesFactor;
+        }
+    }
+}
