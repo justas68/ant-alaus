aa32e696a8b49c0b47f50afa5dba931477cc495a
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2017-11-21 16:50:51 +0200

    Add the feedback WebService functionality

---
+++ Alus/Alus/FeedbackSender.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Alus.Client;
+using Alus.Core.Models;
+
+namespace Alus
+{
+    public class FeedbackSender : IFeedbackSender
+    {
+        private readonly AlusClient _client;
+
+        public FeedbackSender(AlusClient client)
+        {
+            _client = client;
+        }
+
+        public async Task SendAsync(Feedback feedback)
+        {
+            await _client.AddAsync(feedback);
+        }
+    }
+}
