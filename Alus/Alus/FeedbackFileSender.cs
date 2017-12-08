70fa257cd272f509bd5640f7c9e96bc4fc1d0b3f
justas <31662055+justas68@users.noreply.github.com>
2017-12-08 22:52:24 +0200

    f

--- Alus/Alus/FeedbackFileSender.cs
+++ Alus/Alus/FeedbackFileSender.cs
@@ -3,8 +3,8 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace Alus
 {
