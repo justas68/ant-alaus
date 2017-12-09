62e793530a4132979302496406b1062f5b7b1cd8
justas <31662055+justas68@users.noreply.github.com>
2017-12-09 16:34:03 +0200

    update map
    Added right drawer where you can see a list of nearest bars

--- Alus/BeerApplication/NearestBars.cs
+++ Alus/BeerApplication/NearestBars.cs
@@ -10,10 +10,15 @@ using Alus.Core;
 using Alus.Client;
 using System.Collections.Generic;
 using Alus.Core.Models;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.Design.Widget;
+using Android.Views;
 
 namespace BeerApplication
 {
-    [Activity(Label = "Nearest bars")]
+    [Activity(Label = "Nearest bars", Theme = "@style/Theme.DesignDemo")]
     public class NearestBars : Activity, IOnMapReadyCallback, ILocationListener
     {
 
@@ -23,11 +28,13 @@ namespace BeerApplication
         String provider;
         bool firstTime = true;
         float[] hue = new float[22];
+        DrawerLayout drawerLayout;
+        NavigationView navigationView;
+        List<Bar> bars;
 
         public void OnMapReady(GoogleMap googleMap)
         {
             map = googleMap;
-
             googleMap.UiSettings.ZoomControlsEnabled = true;
             googleMap.UiSettings.CompassEnabled = true;
             googleMap.MoveCamera(CameraUpdateFactory.ZoomIn());
@@ -42,6 +49,13 @@ namespace BeerApplication
                 hue[i] = rand.Next(0, 360);
             }
             SetContentView(Resource.Layout.activity_nearest_bars);
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+            var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, Resource.String.drawer_open, Resource.String.drawer_close);
+            drawerLayout.SetDrawerListener(drawerToggle);
+            drawerToggle.SyncState();
+            navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            setupDrawerContent(navigationView);
             MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
             mapFragment.GetMapAsync(this);
             locMgr = (LocationManager)GetSystemService(LocationService);
@@ -82,6 +96,11 @@ namespace BeerApplication
                 lng = currentLocation.Longitude;
                 if (firstTime == true)
                 {
+                    MarkerOptions marker = new MarkerOptions();
+                    marker.SetPosition(new LatLng(lat, lng));
+                    marker.SetTitle("Current location");
+                    marker.SetSnippet("That's the place you are in");
+                    map.AddMarker(marker);
                     CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
                     builder.Target(new LatLng(lat, lng));
                     CameraPosition cameraPosition = builder.Build();
@@ -90,7 +109,7 @@ namespace BeerApplication
                     map.MoveCamera(cameraUpdate);
                     firstTime = false;
                     Alus.NearestBars findBars = new Alus.NearestBars(new AndroidLocationFinder());
-                    List<Bar> bars = findBars.FindBars();
+                    bars = findBars.FindBars();
                     int i = 0;
                     foreach (var bar in bars)
                     {
@@ -102,13 +121,11 @@ namespace BeerApplication
                         tempMarker.SetIcon(BitmapDescriptorFactory.DefaultMarker(hue[i]));
                         map.AddMarker(tempMarker);
                         i++;
+                        var menu = navigationView.Menu;
+                        var menuItem = menu.FindItem(2131362006 + i);
+                        menuItem.SetTitle(bar.Name);
                     }
                 }
-                MarkerOptions marker = new MarkerOptions();
-                marker.SetPosition(new LatLng(lat, lng));
-                marker.SetTitle("Current location");
-                marker.SetSnippet("That's the place you are in");
-                map.AddMarker(marker);
             }
         }
 
@@ -122,6 +139,40 @@ namespace BeerApplication
 
         public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
         {
+        }
+
+        void setupDrawerContent(NavigationView navigationView)
+        {
+            navigationView.NavigationItemSelected += (sender, e) =>
+            {
+                LatLng cord;
+                e.MenuItem.SetChecked(true);
+                if (e.MenuItem.ItemId == 2131362006)
+                {
+                    Double lat, lng;
+                    lat = currentLocation.Latitude;
+                    lng = currentLocation.Longitude;
+                    cord = new LatLng(lat, lng);
+                }
+                else
+                {
+                    var bar = bars[e.MenuItem.ItemId - 2131362007];
+                    String[] cords = bar.Coordinates.Split(',');
+                    cord = new LatLng(Convert.ToDouble(cords[0], System.Globalization.CultureInfo.InvariantCulture), Convert.ToDouble(cords[1], System.Globalization.CultureInfo.InvariantCulture));
+                }
+                CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+                builder.Target(cord);
+                CameraPosition cameraPosition = builder.Build();
+                cameraPosition.Zoom = 18;
+                CameraUpdate cameraUpdate = CameraUpdateFactory.NewCameraPosition(cameraPosition);
+                map.MoveCamera(cameraUpdate);
+                drawerLayout.CloseDrawers();
+            };
+        }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            navigationView.InflateMenu(Resource.Menu.nav_menu); //Navigation Drawer Layout Menu Creation  
+            return true;
         }
     }
 }
